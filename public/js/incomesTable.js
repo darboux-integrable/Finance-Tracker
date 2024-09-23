df0b1a98daec83f39fb6bc2b89a67a29abdd45a8
TR <tylerrobucci@gmail.com>
2024-09-23 09:03:53 -0400

    ethjzghzfdgnzdgn

--- public/js/incomesTable.js
+++ public/js/incomesTable.js
@@ -1,17 +1,20 @@
 import {Tabulator} from 'https://unpkg.com/tabulator-tables@6.2.5/dist/js/tabulator_esm.min.mjs';
 
 
-
 var table = new Tabulator("#incomeDisplayTable", {
     columns:[
-        {id:1, name:"Billy Bob", age:12, gender:"male", height:95, col:"red", dob:"14/05/2010"},
-        {id:2, name:"Jenny Jane", age:42, gender:"female", height:142, col:"blue", dob:"30/07/1954"},
-        {id:3, name:"Steve McAlistaire", age:35, gender:"male", height:176, col:"green", dob:"04/11/1982"},
+        {title:"Name", field:"name", sorter:"string", width:200, editor:true},
+        {title:"Age", field:"age", sorter:"number", hozAlign:"right", formatter:"progress"},
+        {title:"Gender", field:"gender", sorter:"string", cellClick:function(e, cell){console.log("cell click")},},
+        {title:"Height", field:"height", formatter:"star", hozAlign:"center", width:100},
+        {title:"Favourite Color", field:"col", sorter:"string"},
+        {title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
+        {title:"Cheese Preference", field:"cheese", sorter:"boolean", hozAlign:"center", formatter:"tickCross"},
     ],
 });
+var tabledata = [
 
-table.on("tableBuilt", () => {
-});
+];
 
 
 console.log("big mammi");
