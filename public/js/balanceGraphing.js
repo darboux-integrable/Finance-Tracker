56c8b95b678791e5144941b24d6d4cdcda05764f
SonicarMC <tylerrobucci@gmail.com>
2024-10-05 22:20:45 -0400

    Balance stuff - still need to link balance js

---
+++ public/js/balanceGraphing.js
@@ -0,0 +1,38 @@
+// Setting Current Bal Display
+const balDisplay = document.getElementById("currentBalance");
+balDisplay.textContent = ""; // PULL FROM DATABASE
+
+
+// Add up all incomes
+
+// Add up all expenses
+
+// Adding up all incomes and expenses
+balDisplay.textContent = ""; // PULL FROM DATABASE
+
+const dropdownTypeToDay = {
+    onetime: 0,
+    daily: 1,
+    weekly: 7,
+    biweekly: 14,
+    monthly: 31,
+    Quarterly: 90,
+    Biannually: 180,
+    annually: 365
+};
+
+const monthDays = {
+    january: 31,
+    february: 28,
+    march: 31,
+    april: 30,
+    may: 31,
+    june: 30,
+    july: 31,
+    august: 31,
+    september: 30,
+    october: 31,
+    november: 30,
+    december: 31
+};
+
